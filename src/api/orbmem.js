2352e087fb58ef19d7ff777826f2dde0ac3ce4c6
abhis-byte <tetalaharitha@gmail.com>
2025-12-23 23:07:31 +0530

    Update API_BASE to use environment variable

--- src/api/orbmem.js
+++ src/api/orbmem.js
@@ -1,6 +1,6 @@
 import { auth } from "../auth/firebase";
 
-const API_BASE = "http://127.0.0.1:8000/v1";
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 /**
  * Get Firebase ID token (auto-refreshing)
